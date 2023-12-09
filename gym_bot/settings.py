f19173f091efb6af1d0fd032453a83b65b2a97e9
Pramod Gopinath <gopinath.pramod@gmail.com>
2023-12-09 11:25:48 +0530

    debug

--- gym_bot/settings.py
+++ gym_bot/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s4rl+10k(%fwat^#btz*&ocjf*v@1$c4ra246%482y2_f$gjc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
